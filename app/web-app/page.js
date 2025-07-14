3048f399a28d430a357c514744333f1985e52206
EverOnward15 <leiste15@gmail,com>
2025-07-14 15:17:15 +0530

    Disabled SSR

--- app/web-app/page.js
+++ app/web-app/page.js
@@ -1,6 +1,11 @@
-'use client';
 
 import Web3AuthComponent from "@/components/Web3AuthComponent";
+
+// Dynamically import with SSR disabled
+const Web3AuthComponent = dynamic(() => import('@/components/Web3AuthComponent'), {
+  ssr: false,
+});
+
 
 export default function MiniAppPage() {
   return <Web3AuthComponent />;
