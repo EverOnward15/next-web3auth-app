1a5d4fd421c3269d17e84eabf3f85767f8d2ca67
EverOnward15 <leiste15@gmail,com>
2025-07-14 15:21:30 +0530

    Disabled SSR fixes

--- app/web-app/page.js
+++ app/web-app/page.js
@@ -1,3 +1,7 @@
+'use client';
+
+import dynamic from 'next/dynamic';
+
 // Dynamically import with SSR disabled
 const Web3AuthComponent = dynamic(() => import('@/components/Web3AuthComponent'), {
   ssr: false,
