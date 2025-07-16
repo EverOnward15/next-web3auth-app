56655df6c60371b699e03e03e2e4856fa6815c71
EverOnward15 <leiste15@gmail,com>
2025-07-16 12:43:29 +0530

    Moved to single-factor auth -- fixed error

--- app/web-app/page.js
+++ app/web-app/page.js
@@ -1,11 +1,11 @@
 
 'use client';
 
-// import dynamic from 'next/dynamic';
+import dynamic from 'next/dynamic';
 
-// const Web3AuthComponent = dynamic(() => import('@/components/Web3AuthComponent'), {
-//   ssr: false,
-// });
+const Web3AuthComponent = dynamic(() => import('@/components/Web3AuthComponent'), {
+  ssr: false,
+});
 
 export default function MiniAppPage() {
   return <Web3AuthComponent />;
