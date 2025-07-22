8c130437a1b5e0f80828c97c51da520d39bc3681
EverOnward15 <leiste15@gmail,com>
2025-07-22 09:49:04 +0530

    Advanced troubleshoot 15.7 - added test version API

--- app/api/derive/route.js
+++ app/api/derive/route.js
@@ -1,9 +1,9 @@
 //Users/prathameshbhoite/Code/lotus-app/next-web3auth-app/app/api/derive/route.js
 import * as bitcoin from "bitcoinjs-lib";
-import * as ecc from "tiny-secp256k1";
-import { ECPairFactory } from "ecpair";
+// import * as ecc from "tiny-secp256k1";
+// import { ECPairFactory } from "ecpair";
 
-const ECPair = ECPairFactory(ecc);
+// const ECPair = ECPairFactory(ecc);
 
 // export async function POST(req) {
 //   try {
