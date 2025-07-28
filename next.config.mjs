9c5b37c5f2aa5dfd4642483ae11932c979984bda
EverOnward15 <leiste15@gmail,com>
2025-07-28 14:21:09 +0530

    Added fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,7 @@
+// next.config.mjs
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
@@ -5,7 +9,7 @@ const nextConfig = {
       ...config.experiments,
       asyncWebAssembly: true,
     };
-        config.resolve.fallback = {
+    config.resolve.fallback = {
       ...config.resolve.fallback,
       crypto: require.resolve("crypto-browserify"),
       stream: require.resolve("stream-browserify"),
