349ab75dc128ebaaa9d7ffcb233990bc12b8c519
EverOnward15 <leiste15@gmail,com>
2025-07-28 20:45:34 +0530

    Go again 2

--- components/Web3AuthComponent.js
+++ components/Web3AuthComponent.js
@@ -365,7 +365,7 @@ export default function Web3AuthComponent() {
     if (total > sats + fee) {
       psbt.addOutput({ address: fromAddress, value: total - sats - fee });
     }
-
+    alert("hey");
     // 4) sign & validate
     psbt.signAllInputs(keyPair);
     psbt.validateSignaturesOfAllInputs(); // <-- throws if any input failed
