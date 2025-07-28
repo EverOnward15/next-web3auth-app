33e60c9f4769b02bf6f15029eeb0884753cdde4c
EverOnward15 <leiste15@gmail,com>
2025-07-28 14:54:12 +0530

    Added fix 3 -- separate module -- moves again to layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import "../lib/cryptoPatch";
+
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 "use client"; // Only needed if this is inside `app/` directory
 
-import "../lib/cryptoPatch";
 import dynamic from "next/dynamic";
 
 // Dynamically import the component with SSR disabled
