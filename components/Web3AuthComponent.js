52809c5da8597c5addb4070c9d14ec4e0bad2daa
EverOnward15 <leiste15@gmail,com>
2025-07-29 13:05:44 +0530

    Fixes transaction JSON stringify 3

--- components/Web3AuthComponent.js
+++ components/Web3AuthComponent.js
@@ -346,7 +346,7 @@ export default function Web3AuthComponent() {
         index: utxo.vout,
         witnessUtxo: {
           script: output.script,
-          value: BigInt(utxo.value),    // <-- BigInt here
+          value: utxo.value,    // <-- BigInt here
         },
       });
 
